e873db835dd06f15b8021d26259f1e4cc8ab7b9f
matt henley <mattbhenley@gmail.com>
2019-03-13 17:13:42 -0500

    Add files via upload

---
+++ components/weather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Weather = () => {
+    return (
+        <View style={styles.Container}>
+        <View style={styles.headerContainer} />
+        <View style={styles.bodyContainer} />
+        </View>
+    );
+};
+
